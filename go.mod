53515ddab84581ef0b8934e87d2f4384e5eb41a7
nelsonfrank <nelsonfrank741@gmail.com>
2024-12-17 16:34:15 +0300

    feat: Implement data model for user and post stores

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/nelsonfrank/finance-tracker
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/lib/pq v1.10.9 // indirect
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,45 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pg"
 )
 
+type Post struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserId    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
 type PostsStorage struct {
 	db *sql.DB
 }
 
-func (s *PostsStorage) Create(ctx context.Context) error {
+func (s *PostsStorage) Create(ctx context.Context, post *Post) error {
+	query := `
+	INSERT INTO posts (content, title, user_id, tags)
+	VALUES ($1, $2, $3, $4) RETURN id, created_at, updated_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Content,
+		post.Title,
+		post.UserId,
+		pg.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
