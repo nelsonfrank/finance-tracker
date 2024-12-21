3af657a667a79b1542d78de70541737f6ef66fc6
nelsonfrank <nelsonfrank741@gmail.com>
2024-12-21 10:35:49 +0300

    fix: Add port to default db connection string

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,7 +13,7 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/finance-tracker?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost:5438/finance-tracker?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
