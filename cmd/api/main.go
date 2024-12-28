9b9146c3d61c36df6f7c04dd71223554013acf5f
nelsonfrank <nelsonfrank741@gmail.com>
2024-12-28 22:54:15 +0300

    feat: Add mailer internal package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nelsonfrank/finance-tracker/internal/auth"
 	"github.com/nelsonfrank/finance-tracker/internal/db"
 	"github.com/nelsonfrank/finance-tracker/internal/env"
+	"github.com/nelsonfrank/finance-tracker/internal/mailer"
 	"github.com/nelsonfrank/finance-tracker/internal/store"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -15,7 +16,9 @@ import (
 func main() {
 
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr:        env.GetString("ADDR", ":8080"),
+		frontendURL: env.GetString("FRONTEND_URL", "http://localhost:3000"),
+		env:         env.GetString("ENV", "development"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "host=localhost user=admin password=adminpassword dbname=finance-tracker port=5438  sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
@@ -38,6 +41,16 @@ func main() {
 				exp:             time.Second * 5,
 				iss:             "financial-tracker"},
 		},
+		mail: mailConfig{
+			exp:       time.Hour * 24 * 3, // 3 days
+			fromEmail: env.GetString("FROM_EMAIL", ""),
+			sendGrid: sendGridConfig{
+				apiKey: env.GetString("SENDGRID_API_KEY", ""),
+			},
+			mailTrap: mailTrapConfig{
+				apiKey: env.GetString("MAILTRAP_API_KEY", ""),
+			},
+		},
 	}
 
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
@@ -54,11 +67,20 @@ func main() {
 		cfg.mfa.token.iss,
 		cfg.mfa.token.iss,
 	)
+
+	// Mailer
+	// mailer := mailer.NewSendgrid(cfg.mail.sendGrid.apiKey, cfg.mail.fromEmail)
+	mailtrap, err := mailer.NewMailTrapClient(cfg.mail.mailTrap.apiKey, cfg.mail.fromEmail)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := &application{
 		config:        cfg,
 		store:         store,
 		db:            db,
 		authenticator: jwtAuthenticator,
+		mailer:        mailtrap,
 	}
 
 	mux := app.mount()
