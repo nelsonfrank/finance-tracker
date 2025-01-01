0b21afc703ca3ae487c99ec676d2dcf96d36f8c7
nelsonfrank <nelsonfrank741@gmail.com>
2025-01-01 19:18:56 +0300

    fix(ui): Add terms and privacy policy in auth layout

--- web/src/app/auth/layout.tsx
+++ web/src/app/auth/layout.tsx
@@ -15,6 +15,10 @@ export default function AuthLayout({
           Cash Flow
         </a>
         {children}
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
       </div>
     </div>
   );
