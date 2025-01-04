db6dd9232e78adcf9a34cc922fa1b9553acb5cf5
nelsonfrank <nelsonfrank741@gmail.com>
2025-01-04 17:19:19 +0300

    feat(ui): Add validation for env variables
    - Using @t3-oss/env-nextjs package

---
+++ web/src/env/client.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  emptyStringAsUndefined: true,
+});
---
+++ web/src/env/server.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    API_BASE_URL: z.string().url(),
+  },
+
+  experimental__runtimeEnv: process.env,
+});
