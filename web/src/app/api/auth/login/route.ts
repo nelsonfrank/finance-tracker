a4a55c054e4c428d174be3e5e37bc318252971ae
nelsonfrank <nelsonfrank741@gmail.com>
2025-01-24 00:24:58 +0300

    feat: Store access token in in-memory

--- web/src/app/api/auth/login/route.ts
+++ web/src/app/api/auth/login/route.ts
@@ -1,17 +1,61 @@
 import { cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 import * as API from "@/data/backend/api";
+import redis from "redis";
 
 export async function POST(req: NextRequest) {
   const payload = await req.json();
 
+  const redisClient = redis.createClient();
   const response = await API.loginAPI(payload);
   const data = response.data;
 
-  const cookieStore = await cookies();
-  const expirationDate = new Date();
-  expirationDate.setMinutes(expirationDate.getMinutes() + 5);
+  // store access token in redis
+  storeAuthTokenInServer(redisClient, data.access_token);
 
+  //store access token in browser
+  storeAuthTokenInBrowser(data);
+
+  return Response.json(data.user);
+}
+
+const storeAuthTokenInServer = async (
+  redisClient: any,
+  accessToken: string
+) => {
+  try {
+    // Connect to Redis
+    await redisClient.connect();
+
+    // Store Access Token with expiration
+    await redisClient.set("accessToken", accessToken, {
+      EX: 15 * 60, // Expiration in 15 mins
+    });
+    // Store Refresh Token with expiration
+    await redisClient.set("accessToken", accessToken, {
+      EX: 15 * 60 * 60 * 24, // Expiration in 15 days
+    });
+
+    console.log("Access token stored successfully!");
+  } catch (err) {
+    console.error("Error storing access token in Redis:", err);
+  } finally {
+    // Close Redis connection
+    await redisClient.disconnect();
+  }
+};
+
+const storeAuthTokenInBrowser = async (data: API.loginResponse) => {
+  const cookieStore = await cookies();
+
+  const expirationDate = new Date();
+  const refreshTokenExpirationDate = new Date();
+
+  // Add 5 seconds to `expirationDate`
+  expirationDate.setDate(refreshTokenExpirationDate.getDate() + 1);
+
+  // Add 7 days to `refreshTokenExpirationDate`
+  refreshTokenExpirationDate.setDate(refreshTokenExpirationDate.getDate() + 7);
   cookieStore.set("access_token", data.access_token, {
     path: "/",
     expires: expirationDate,
@@ -22,10 +66,9 @@ export async function POST(req: NextRequest) {
 
   cookieStore.set("refresh_token", data.refresh_token, {
     path: "/",
-    expires: expirationDate,
+    expires: refreshTokenExpirationDate,
     httpOnly: true,
     secure: true, // Set to true in production
     sameSite: "lax",
   });
-  return Response.json(data.user);
-}
+};
